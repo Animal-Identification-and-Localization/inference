f76df4f92d60b722b05707abefd8c087e2ebd134
Aurimas Balciunas <56936689+AurimasBalciunas@users.noreply.github.com>
2022-10-09 22:39:23 -0400

    detector.py, use_detector.py: adding object list printing

--- detector.py
+++ detector.py
@@ -28,6 +28,21 @@ class Detector:
             y1 = bounding_box.ymax
             canvas.rectangle([(x0, y0), (x1, y1)], outline='red')
             canvas.text((x0 + 10, y0 + 10), "%s\n%.3f" % (labels.get(id,id), score), fill="red")
+    
+    def get_bbox_list(self, objects_detected, labels):
+        object_list = []
+        for object in objects_detected:
+            bounding_box = object.bbox
+            id = object.id
+            score = object.score
+            x0 = bounding_box.xmin
+            x1 = bounding_box.xmax
+            y0 = bounding_box.ymin
+            y1 = bounding_box.ymax
+            label = labels.get(id, id)
+            list_entry = [label, score, x0, x1, y0, y1]
+            object_list.push(list_entry)
+        return object_list
 
     def run_detection(self, input_image_path, output_image_path):
         #Rescaling Image
@@ -49,4 +64,7 @@ class Detector:
         image = image.convert("RGB")
         self.draw_boxes(image, objects_detected, self.labels)
         image.save(output_image_path)
-        image.show()
+
+        #Printing bounding box info
+        object_list = self.get_bbox_list(objects_detected, self.labels)
+        return object_list
--- use_detector.py
+++ use_detector.py
@@ -7,7 +7,9 @@ def main():
     output_image_path = "../images/Doutput.jpg"
 
     d1 = Detector(model_path, labels_path, .4)
-    d1.run_detection(input_image_path, output_image_path)
+    object_list = d1.run_detection(input_image_path, output_image_path)
+    print(object_list)
+    
 
 if __name__ == "__main__":
     main()
